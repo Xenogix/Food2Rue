42ade67a37fe1cbe7323596ecaa0320fb52bbc4e
Arthur Menétrey <54710977+Xenogix@users.noreply.github.com>
2023-12-05 22:14:16 +0100

    Fix script

--- DDL-DML.sql
+++ DDL-DML.sql
@@ -1,33 +1,37 @@
-DROP DATABASE IF EXISTS `food2rue`;
-CREATE DATABASE IF NOT EXISTS `food2rue` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `food2rue`;
+DROP schema IF EXISTS food2rue CASCADE;
+CREATE SCHEMA food2rue;
 
-CREATE TABLE pays (
+DROP TABLE IF EXISTS food2rue.pays;
+CREATE TABLE  food2rue.pays (
     sigle VARCHAR(2),
     nom VARCHAR(255),
     PRIMARY KEY (sigle)
 );
 
-CREATE TABLE media (
-  id INT,
+DROP TABLE IF EXISTS food2rue.media;
+CREATE TABLE  food2rue.media (
+  id SERIAL,
   url_source VARCHAR(255),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE image (
-  id INT,
+DROP TABLE IF EXISTS food2rue.image;
+CREATE TABLE  food2rue.image (
+  id SERIAL,
   PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES media(id)
+  FOREIGN KEY (id) REFERENCES food2rue.media(id)
 );
 
-CREATE TABLE video (
-  id INT,
+DROP TABLE IF EXISTS food2rue.video;
+CREATE TABLE  food2rue.video (
+  id SERIAL,
   PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES media(id)
+  FOREIGN KEY (id) REFERENCES food2rue.media(id)
 );
 
-CREATE TABLE utilisateur (
-  id INT,
+DROP TABLE IF EXISTS food2rue.utilisateur;
+CREATE TABLE  food2rue.utilisateur (
+  id SERIAL,
   nom VARCHAR(255),
   prénom VARCHAR(255),
   email VARCHAR(255),
@@ -38,49 +42,20 @@ CREATE TABLE utilisateur (
   description VARCHAR(255),
   fk_pays VARCHAR(2),
   PRIMARY KEY (id),
-  FOREIGN KEY (fk_pays) REFERENCES pays(sigle),
-  FOREIGN KEY (fk_photo_profil) REFERENCES image(id)
+  FOREIGN KEY (fk_pays) REFERENCES food2rue.pays(sigle),
+  FOREIGN KEY (fk_photo_profil) REFERENCES food2rue.image(id)
 );
 
-CREATE TABLE administrateur (
-  id INT,
+DROP TABLE IF EXISTS food2rue.administrateur;
+CREATE TABLE  food2rue.administrateur (
+  id SERIAL,
   PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES utilisateur(id)
+  FOREIGN KEY (id) REFERENCES food2rue.utilisateur(id)
 );
 
-CREATE TABLE publication (
-  id INT,
-  texte VARCHAR(255),
-  date_publication DATE,
-  fk_parent INT,
-  fk_utilisateur INT NOT NULL,
-  fk_video INT,
-  fk_recette INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (fk_parent) REFERENCES publication(id),
-  FOREIGN KEY (fk_utilisateur) REFERENCES utilisateur(id),
-  FOREIGN KEY (fk_video) REFERENCES video(id),
-  FOREIGN KEY (fk_recette) REFERENCES recette(id)
-);
-
-CREATE TABLE publication_image (
-  fk_publication INT,
-  fk_image INT,
-  PRIMARY KEY (fk_publication, fk_image),
-  FOREIGN KEY (fk_publication) REFERENCES publication(id),
-  FOREIGN KEY (fk_image) REFERENCES image(id)
-);
-
-CREATE TABLE aime_publication_utilisateur (
-  fk_publication INT,
-  fk_utilisateur INT,
-  PRIMARY KEY (fk_publication, fk_utilisateur),
-  FOREIGN KEY (fk_publication) REFERENCES publication(id),
-  FOREIGN KEY (fk_utilisateur) REFERENCES utilisateur(id)
-);
-
-CREATE TABLE recette (
-  id INT,
+DROP TABLE IF EXISTS food2rue.recette;
+CREATE TABLE  food2rue.recette (
+  id SERIAL,
   nom VARCHAR(255),
   temps_preparation INT,
   temps_cuisson INT,
@@ -91,52 +66,92 @@ CREATE TABLE recette (
   fk_video INT,
   fk_pays VARCHAR(2),
   PRIMARY KEY (id),
-  FOREIGN KEY (fk_utilisateur) REFERENCES utilisateur(id),
-  FOREIGN KEY (fk_video) REFERENCES video(id),
-  FOREIGN KEY (fk_pays) REFERENCES pays(sigle)
+  FOREIGN KEY (fk_utilisateur) REFERENCES food2rue.utilisateur(id),
+  FOREIGN KEY (fk_video) REFERENCES food2rue.video(id),
+  FOREIGN KEY (fk_pays) REFERENCES food2rue.pays(sigle)
 );
 
-CREATE TABLE note (
+DROP TABLE IF EXISTS food2rue.publication;
+CREATE TABLE  food2rue.publication (
+  id SERIAL,
+  texte VARCHAR(255),
+  date_publication DATE,
+  fk_parent INT,
+  fk_utilisateur INT NOT NULL,
+  fk_video INT,
+  fk_recette INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (fk_parent) REFERENCES food2rue.publication(id),
+  FOREIGN KEY (fk_utilisateur) REFERENCES food2rue.utilisateur(id),
+  FOREIGN KEY (fk_video) REFERENCES food2rue.video(id),
+  FOREIGN KEY (fk_recette) REFERENCES food2rue.recette(id)
+);
+
+DROP TABLE IF EXISTS food2rue.publication_image;
+CREATE TABLE  food2rue.publication_image (
+  fk_publication INT,
+  fk_image INT,
+  PRIMARY KEY (fk_publication, fk_image),
+  FOREIGN KEY (fk_publication) REFERENCES food2rue.publication(id),
+  FOREIGN KEY (fk_image) REFERENCES food2rue.image(id)
+);
+
+DROP TABLE IF EXISTS food2rue.aime_publication_utilisateur;
+CREATE TABLE  food2rue.aime_publication_utilisateur (
+  fk_publication INT,
+  fk_utilisateur INT,
+  PRIMARY KEY (fk_publication, fk_utilisateur),
+  FOREIGN KEY (fk_publication) REFERENCES food2rue.publication(id),
+  FOREIGN KEY (fk_utilisateur) REFERENCES food2rue.utilisateur(id)
+);
+
+DROP TABLE IF EXISTS food2rue.note;
+CREATE TABLE  food2rue.note (
   fk_utilisateur INT,
   fk_publication INT,
   note INT,
   PRIMARY KEY (fk_utilisateur, fk_publication),
-  FOREIGN KEY (fk_utilisateur) REFERENCES utilisateur(id),
-  FOREIGN KEY (fk_publication) REFERENCES publication(id)
+  FOREIGN KEY (fk_utilisateur) REFERENCES food2rue.utilisateur(id),
+  FOREIGN KEY (fk_publication) REFERENCES food2rue.publication(id)
 );
 
-CREATE TABLE recette_image (
+DROP TABLE IF EXISTS food2rue.recette_image;
+CREATE TABLE  food2rue.recette_image (
   fk_recette INT,
   fk_image INT,
   PRIMARY KEY (fk_recette, fk_image),
-  FOREIGN KEY (fk_recette) REFERENCES recette(id),
-  FOREIGN KEY (fk_image) REFERENCES image(id)
+  FOREIGN KEY (fk_recette) REFERENCES food2rue.recette(id),
+  FOREIGN KEY (fk_image) REFERENCES food2rue.image(id)
 );
 
-CREATE TABLE tag (
-  id INT,
+DROP TABLE IF EXISTS food2rue.tag;
+CREATE TABLE  food2rue.tag (
+  id SERIAL,
   nom VARCHAR(255),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE publication_tag (
+DROP TABLE IF EXISTS food2rue.publication_tag;
+CREATE TABLE  food2rue.publication_tag (
   fk_publication INT,
   fk_tag INT,
   PRIMARY KEY (fk_publication, fk_tag),
-  FOREIGN KEY (fk_publication) REFERENCES publication(id),
-  FOREIGN KEY (fk_tag) REFERENCES tag(id)
+  FOREIGN KEY (fk_publication) REFERENCES food2rue.publication(id),
+  FOREIGN KEY (fk_tag) REFERENCES food2rue.tag(id)
 );
 
-CREATE TABLE recette_tag (
+DROP TABLE IF EXISTS food2rue.recette_tag;
+CREATE TABLE  food2rue.recette_tag (
   fk_recette INT,
   fk_tag INT,
   PRIMARY KEY (fk_recette, fk_tag),
-  FOREIGN KEY (fk_recette) REFERENCES recette(id),
-  FOREIGN KEY (fk_tag) REFERENCES tag(id)
+  FOREIGN KEY (fk_recette) REFERENCES food2rue.recette(id),
+  FOREIGN KEY (fk_tag) REFERENCES food2rue.tag(id)
 );
 
-CREATE TABLE ajoutable (
-  id INT,
+DROP TABLE IF EXISTS food2rue.ajoutable;
+CREATE TABLE  food2rue.ajoutable (
+  id SERIAL,
   nom VARCHAR(255),
   description VARCHAR(255),
   date_publication DATE,
@@ -145,56 +160,60 @@ CREATE TABLE ajoutable (
   fk_administrateur INT,
   est_valide BOOLEAN,
   PRIMARY KEY (id),
-  FOREIGN KEY (fk_administrateur) REFERENCES administrateur(id),
-  FOREIGN KEY (fk_utilisateur) REFERENCES utilisateur(id),
-  FOREIGN KEY (fk_image) REFERENCES image(id)
+  FOREIGN KEY (fk_administrateur) REFERENCES food2rue.administrateur(id),
+  FOREIGN KEY (fk_utilisateur) REFERENCES food2rue.utilisateur(id),
+  FOREIGN KEY (fk_image) REFERENCES food2rue.image(id)
 );
 
-CREATE TABLE ingredient (
-  id INT,
+DROP TABLE IF EXISTS food2rue.ingredient;
+CREATE TABLE  food2rue.ingredient (
+  id SERIAL,
   PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES ajoutable(id)
+  FOREIGN KEY (id) REFERENCES food2rue.ajoutable(id)
 );
 
-CREATE TABLE recette_ingredient (
+DROP TABLE IF EXISTS food2rue.recette_ingredient;
+CREATE TABLE  food2rue.recette_ingredient (
   fk_recette INT,
   fk_ingredient INT,
   quantite VARCHAR(255),
   PRIMARY KEY (fk_recette, fk_ingredient),
-  FOREIGN KEY (fk_recette) REFERENCES recette(id),
-  FOREIGN KEY (fk_ingredient) REFERENCES ingredient(id)
+  FOREIGN KEY (fk_recette) REFERENCES food2rue.recette(id),
+  FOREIGN KEY (fk_ingredient) REFERENCES food2rue.ingredient(id)
 );
 
-CREATE TABLE allergene (
+DROP TABLE IF EXISTS food2rue.allergene;
+CREATE TABLE  food2rue.allergene (
   nom VARCHAR(255),
   PRIMARY KEY (nom)
 );
 
-CREATE TABLE ingredient_allergene (
+DROP TABLE IF EXISTS food2rue.ingredient_allergene;
+CREATE TABLE  food2rue.ingredient_allergene (
   fk_ingredient INT,
   fk_allergene VARCHAR(255),
   PRIMARY KEY (fk_ingredient, fk_allergene),
-  FOREIGN KEY (fk_ingredient) REFERENCES ingredient(id),
-  FOREIGN KEY (fk_allergene) REFERENCES allergene(nom)
+  FOREIGN KEY (fk_ingredient) REFERENCES food2rue.ingredient(id),
+  FOREIGN KEY (fk_allergene) REFERENCES food2rue.allergene(nom)
 );
 
-CREATE TABLE ustensile (
-  id INT,
-  nom VARCHAR(255),
+DROP TABLE IF EXISTS food2rue.ustensile;
+CREATE TABLE  food2rue.ustensile (
+  id SERIAL,
   PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES ajoutable(id)
+  FOREIGN KEY (id) REFERENCES food2rue.ajoutable(id)
 );
 
-CREATE TABLE recette_ustensile (
+DROP TABLE IF EXISTS food2rue.recette_ustensile;
+CREATE TABLE  food2rue.recette_ustensile (
   fk_recette INT,
   fk_ustensile INT,
   PRIMARY KEY (fk_recette, fk_ustensile),
-  FOREIGN KEY (fk_recette) REFERENCES recette(id),
-  FOREIGN KEY (fk_ustensile) REFERENCES ustensile(id)
+  FOREIGN KEY (fk_recette) REFERENCES food2rue.recette(id),
+  FOREIGN KEY (fk_ustensile) REFERENCES food2rue.ustensile(id)
 );
 
-
-INSERT INTO pays (sigle, nom)
+INSERT INTO food2rue.pays (sigle, nom)
 VALUES ('AF', 'Afghanistan'),
        ('AL', 'Albanie'),
        ('DZ', 'Algérie'),
@@ -436,7 +455,7 @@ VALUES ('AF', 'Afghanistan'),
        ('ZW', 'Zimbabwe');
 
 
-INSERT INTO tag (id, nom)
+INSERT INTO food2rue.tag (id, nom)
 VALUES (1, 'digest'),
        (2, 'healthy'),
        (3, 'fit'),
@@ -499,21 +518,23 @@ VALUES (1, 'digest'),
        (60, 'sans viande de requin'),
        (61, 'torture de poulet');
 
-INSERT INTO utilisateur (nom, prénom, email, fk_photo_profil, pseudo, date_naissance, date_creation_profil, description, fk_pays)
-VALUES ('John', 'Doe', 'john.doe@example.com', 1, 'johndoe', '1990-01-01', '2022-01-01', 'Lorem ipsum dolor sit amet', 'US'),
-       ('Jane', 'Smith', 'jane.smith@example.com', 1, 'janesmith', '1995-02-15', '2022-01-01', 'Lorem ipsum dolor sit amet', 'GB'),
-       ('Mike', 'Johnson', 'mike.johnson@example.com', 1, 'mikejohnson', '1985-06-30', '2022-01-01', 'Lorem ipsum dolor sit amet', 'CA'),
-       ('Sarah', 'Williams', 'sarah.williams@example.com', 1, 'sarahwilliams', '1992-11-20', '2022-01-01', 'Lorem ipsum dolor sit amet', 'AU'),
-       ('David', 'Brown', 'david.brown@example.com', 1, 'davidbrown', '1998-09-10', '2022-01-01', 'Lorem ipsum dolor sit amet', 'FR');
+INSERT INTO food2rue.media (id, url_source)
+VALUES (1, './img/photo_profile_default.png');
 
+INSERT INTO food2rue.image (id)
+VALUES (1);
 
-INSERT INTO administrateur (id)
-VALUES ('Admin', NULL, 'admin@example.com', 1, 'admin', '1990-01-01', '2022-01-01', 'Lorem ipsum dolor sit amet', 'US');
+INSERT INTO food2rue.utilisateur (id,nom, prénom, email, fk_photo_profil, pseudo, date_naissance, date_creation_profil, description, fk_pays)
+VALUES (1, 'John', 'Doe', 'john.doe@example.com', 1, 'johndoe', '1990-01-01', '2022-01-01', 'Lorem ipsum dolor sit amet', 'US'),
+       (2, 'Jane', 'Smith', 'jane.smith@example.com', 1, 'janesmith', '1995-02-15', '2022-01-01', 'Lorem ipsum dolor sit amet', 'GB'),
+       (3, 'Mike', 'Johnson', 'mike.johnson@example.com', 1, 'mikejohnson', '1985-06-30', '2022-01-01', 'Lorem ipsum dolor sit amet', 'CA'),
+       (4, 'Sarah', 'Williams', 'sarah.williams@example.com', 1, 'sarahwilliams', '1992-11-20', '2022-01-01', 'Lorem ipsum dolor sit amet', 'AU'),
+       (5, 'David', 'Brown', 'david.brown@example.com', 1, 'davidbrown', '1998-09-10', '2022-01-01', 'Lorem ipsum dolor sit amet', 'FR');
 
-INSERT INTO image (url_source)
-VALUES ('./img/photo_profile_default.png');
+INSERT INTO food2rue.administrateur (id)
+VALUES (1);
 
-INSERT INTO allergene (nom)
+INSERT INTO food2rue.allergene (nom)
 VALUES ('Oeufs'),
        ('Lactose'),
        ('Mollusques'),
@@ -529,53 +550,93 @@ VALUES ('Oeufs'),
        ('Celeri'),
        ('Moutarde');
 
-INSERT INTO image (url_source)
-VALUES ('./img/Cuillere.png'),
-       ('./img/Fourchette.png'),
-       ('./img/Couteau.png'),
-       ('./img/Assiette.png'),
-       ('./img/Casserole.png'),
-       ('./img/Poele.png'),
-       ('./img/Pince.png'),
-       ('./img/Eplucheuse.png'),
-       ('./img/CuiseurVapeur.png');
+INSERT INTO food2rue.media (id, url_source)
+VALUES (2,'./img/Cuillere.png'),
+       (3,'./img/Fourchette.png'),
+       (4,'./img/Couteau.png'),
+       (5,'./img/Assiette.png'),
+       (6,'./img/Casserole.png'),
+       (7,'./img/Poele.png'),
+       (8,'./img/Pince.png'),
+       (9,'./img/Eplucheuse.png'),
+       (10,'./img/CuiseurVapeur.png');
+      
+INSERT INTO food2rue.image (id)
+VALUES (2),
+	   (3),
+	   (4),
+	   (5),
+	   (6),
+	   (7),
+	   (8),
+	   (9),
+	   (10);
 
-INSERT INTO ustensile (nom, description, date_expiration, fk_image, fk_utilisateur, fk_administrateur, fk_pays, actif)
-VALUES ('Cuillère', 'Fourchette sans dent pour manger les soupes et les desserts', '2022-12-01', 2, 1, 6, true),
-       ('Fourchette', 'Fourchette avec 4 dents pour manger les plats', '2022-12-01', 3, 1, 6, true),
-       ('Couteau', 'Couteau pour couper les aliments', '2022-12-01', 4, 1, 6, true),
-       ('Assiette', 'Assiette pour mettre les aliments', '2022-12-01', 5, 1, 6, true),
-       ('Casserole', 'Casserole pour faire cuire les aliments', '2022-12-01', 6, 1, 6, true),
-       ('Poêle à frire', 'Poêle à frire pour faire cuire les aliments', '2022-12-01', 7, 1, 6, true),
-       ('Pince', 'Pince pour attraper les aliments', '2022-12-01', 8, 1, 6, true),
-       ('Éplucheuse', 'Éplucheuse pour éplucher les aliments', '2022-12-01', 9, 1, 6, true),
-       ('Cuiseur vapeur', 'Cuiseur vapeur pour cuire les aliments à la vapeur', '2022-12-01', 10, 1, 6, true);
+INSERT INTO food2rue.ajoutable (id, nom, description, date_publication, fk_image, fk_utilisateur, fk_administrateur, est_valide)
+VALUES (1, 'Cuillère', 'Fourchette sans dent pour manger les soupes et les desserts', '2022-12-01', 2, 1, 1, true),
+       (2, 'Fourchette', 'Fourchette avec 4 dents pour manger les plats', '2022-12-01', 3, 2, 1, true),
+       (3, 'Couteau', 'Couteau pour couper les aliments', '2022-12-01', 4, 3, 1, true),
+       (4, 'Assiette', 'Assiette pour mettre les aliments', '2022-12-01', 5, 4, 1, true),
+       (5, 'Casserole', 'Casserole pour faire cuire les aliments', '2022-12-01', 6, 5, 1, true),
+       (6, 'Poêle à frire', 'Poêle à frire pour faire cuire les aliments', '2022-12-01', 7, 1, 1, true),
+       (7, 'Pince', 'Pince pour attraper les aliments', '2022-12-01', 8, 2, 1, true),
+       (8, 'Éplucheuse', 'Éplucheuse pour éplucher les aliments', '2022-12-01', 9, 3, 1, true),
+       (9, 'Cuiseur vapeur', 'Cuiseur vapeur pour cuire les aliments à la vapeur', '2022-12-01', 10, 4, 1, true);
+      
+INSERT INTO food2rue.ustensile (id)
+VALUES (1),
+       (2),
+       (3),
+       (4),
+       (5),
+       (6),
+       (7),
+       (8),
+       (9);
 
-INSERT INTO image (url_source)
-VALUES ('./img/pouletFermier.png'),
-       ('./img/curry.png'),
-       ('./img/riz.png'),
-       ('./img/schnaps.png');
+INSERT INTO food2rue.media (id, url_source)
+VALUES (11,'./img/pouletFermier.png'),
+       (12,'./img/curry.png'),
+       (13,'./img/riz.png'),
+       (14,'./img/schnaps.png');
+      
+INSERT INTO food2rue.image (id)
+VALUES (11),
+	   (12),
+	   (13),
+	   (14);
 
-INSERT INTO ingredient (nom, description, date_expiration, fk_image, fk_utilisateur, fk_administrateur, fk_pays, actif)
-VALUES ('Poulet', 'Poulet de ferme', '2022-12-01', 11, 1, 6, true),
-       ('Curry', 'Curry de la marque X', '2022-12-01', 12, 1, 6, true),
-       ('Riz', 'Riz de la marque Y', '2022-12-01', 13, 1, 6, true),
-       ('Schnaps', 'Schnaps de la marque Z', '2022-12-01', 14, 1, 6, true);
+INSERT INTO food2rue.ajoutable (id, nom, description, date_publication, fk_image, fk_utilisateur, fk_administrateur, est_valide)
+VALUES (10, 'Poulet', 'Poulet de ferme', '2022-12-01', 11, 2, 1, true),
+       (11, 'Curry', 'Curry de la marque X', '2022-12-01', 12, 3, 1, true),
+       (12, 'Riz', 'Riz de la marque Y', '2022-12-01', 13, 4, 1, true),
+       (13, 'Schnaps', 'Schnaps de la marque Z', '2022-12-01', 14, 5, 1, true);
+      
+INSERT INTO food2rue.ingredient (id)
+VALUES (10),
+       (11),
+       (12),
+       (13);
+      
+INSERT INTO food2rue.media (id, url_source)
+VALUES (15,'./img/pouletCurry.mp4');
+      
+INSERT INTO food2rue.video (id)
+VALUES (15);
 
-INSERT INTO recette (nom, temps_preparation, temps_cuisson, temps_repos, date_creation, etape, fk_utilisateur, fk_video, fk_pays, actif, langue)
-VALUES ('Poulet au curry', 30, 30, 0, '2022-12-01', '# Etape 1 : eplucher le poulet\n\n#Etape 2 : Plonger le poulet encore vivant dans du curry boullant\n\n# Etape 3 : Server vos invité avec du riz et du schnaps', 1, 0, 'FR')
+INSERT INTO food2rue.recette (nom, temps_preparation, temps_cuisson, temps_repos, date_creation, etape, fk_utilisateur, fk_video, fk_pays)
+VALUES ('Poulet au curry', 30, 30, 0, '2022-12-01', '# Etape 1 : epluchez le poulet\n\n#Etape 2 : Plonger le poulet encore vivant dans du curry boullant\n\n# Etape 3 : Servez vos invités avec du riz et du schnaps', 1, 15, 'FR');
 
-INSERT INTO recette_tag (fk_recette, fk_tag)
-VALUES (1, 61)
+INSERT INTO food2rue.recette_tag (fk_recette, fk_tag)
+VALUES (1, 61);
 
-INSERT INTO recette_ingredient (fk_recette, fk_ingredient, quantite)
-VALUES (1, 1, '1 poulet'),
-       (1, 2, '5 cuillère à soupe'),
-       (1, 3, '2 dose'),
-       (1, 4, '1 verre');
+INSERT INTO food2rue.recette_ingredient (fk_recette, fk_ingredient, quantite)
+VALUES (1, 10, '1 poulet'),
+       (1, 11, '5 cuillère à soupe'),
+       (1, 12, '2 dose'),
+       (1, 13, '1 verre');
 
-INSERT INTO recette_ustensile (fk_recette, fk_ustensile)
+INSERT INTO food2rue.recette_ustensile (fk_recette, fk_ustensile)
 VALUES (1, 1),
        (1, 2),
        (1, 3),
@@ -586,25 +647,24 @@ VALUES (1, 1),
        (1, 8),
        (1, 9);
 
-
-
-INSERT INTO publication (texte, date_publication, fk_parent, fk_utilisateur, fk_video, fk_recette)
+INSERT INTO food2rue.publication (texte, date_publication, fk_parent, fk_utilisateur, fk_video, fk_recette)
 VALUES ('Ma première recette ;D', '2022-12-01', NULL, 1, NULL, 1),
        ('Good job', '2022-12-01', 1, 2, NULL, NULL),
        ('Merci', '2022-12-01', 2, 1, NULL, NULL),
        ('Je vais essayer', '2022-12-01', 1, 3, NULL, NULL),
        ('Regarde mon resulta', '2022-12-01', 1, 5, NULL, NULL);
 
+INSERT INTO food2rue.media (id, url_source)
+VALUES (16,'./img/maversion124324.png');
 
-INSERT INTO image (url_source)
-VALUES ('./img/maversion124324.png')
+INSERT INTO food2rue.image (id)
+VALUES (16);
 
-INSERT INTO publication_image (fk_publication, fk_image)
-VALUES (5, 15);
+INSERT INTO food2rue.publication_image (fk_publication, fk_image)
+VALUES (5, 16);
 
-INSERT INTO aime_publication_utilisateur (fk_publication, fk_utilisateur)
+INSERT INTO food2rue.aime_publication_utilisateur (fk_publication, fk_utilisateur)
 VALUES (1, 2),
-       (1, 3),
        (1, 3),
        (1, 5),
        (5, 1),
